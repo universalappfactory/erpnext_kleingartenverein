5053f945eaa666a8f5d8269580592df6cbe36634
dirk <info@universalappfactory.de>
2023-07-01 13:40:10 +0000

    fix: stye fixes

--- erpnext_kleingartenverein/www/utils.py
+++ erpnext_kleingartenverein/www/utils.py
@@ -29,6 +29,13 @@ def add_default_context_data(context):
         context.club_name = club_settings.club_name
         context.club_city = club_settings.club_city
         context.default_header_image = club_settings.default_header_image
+        context.club_mail_contact = club_settings.club_mail_contact
+        context.club_phone_contact = club_settings.club_phone_contact
+        context.club_contact_message = club_settings.club_contact_message
+        context.club_phone_message = club_settings.club_phone_message
+        context.club_mail_message = club_settings.club_mail_message
+        context.service_section_background = club_settings.service_section_background
+
     except DoesNotExistError:
         pass
 
