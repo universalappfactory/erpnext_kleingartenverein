5c3bbe20c837b26d1c4ab0c1b8c3d9563b797682
dirk <info@universalappfactory.de>
2023-07-17 12:34:59 +0000

    ci: modified install script

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -1,67 +1,52 @@
 #!/bin/bash
-
 set -e
-
 cd ~ || exit
 
-sudo apt update && sudo apt install redis-server libcups2-dev
+echo "Setting Up Bench..."
 
 pip install frappe-bench
+bench -v init frappe-bench --skip-assets --python "$(which python)"
+cd ./frappe-bench || exit
 
-githubbranch=${GITHUB_BASE_REF:-${GITHUB_REF##*/}}
-frappeuser=${FRAPPE_USER:-"frappe"}
-frappebranch=${FRAPPE_BRANCH:-$githubbranch}
+bench -v setup requirements
 
-git clone "https://github.com/${frappeuser}/frappe" --branch "$feature/{frappebranch}" --depth 1
-bench init --skip-assets --frappe-path ~/frappe --python "$(which python)" frappe-bench
+echo "Setting Up erpnext_kleingartenverein App..."
+bench get-app erpnext_kleingartenverein "${GITHUB_WORKSPACE}"
+
+echo "Setting Up Sites & Database..."
 
 mkdir ~/frappe-bench/sites/test_site
+cp "${GITHUB_WORKSPACE}/.github/helper/site_config.json" ~/frappe-bench/sites/test_site/site_config.json
 
-if [ "$DB" == "mariadb" ];then
-    cp -r "${GITHUB_WORKSPACE}/.github/helper/site_config_mariadb.json" ~/frappe-bench/sites/test_site/site_config.json
-else
-    cp -r "${GITHUB_WORKSPACE}/.github/helper/site_config_postgres.json" ~/frappe-bench/sites/test_site/site_config.json
-fi
+mariadb --host 127.0.0.1 --port 3306 -u root -p123 -e "SET GLOBAL character_set_server = 'utf8mb4'";
+mariadb --host 127.0.0.1 --port 3306 -u root -p123 -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'";
 
+mariadb --host 127.0.0.1 --port 3306 -u root -p123 -e "CREATE DATABASE test_frappe";
+mariadb --host 127.0.0.1 --port 3306 -u root -p123 -e "CREATE USER 'test_frappe'@'localhost' IDENTIFIED BY 'test_frappe'";
+mariadb --host 127.0.0.1 --port 3306 -u root -p123 -e "GRANT ALL PRIVILEGES ON \`test_frappe\`.* TO 'test_frappe'@'localhost'";
 
-if [ "$DB" == "mariadb" ];then
-    mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL character_set_server = 'utf8mb4'"
-    mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'"
-
-    mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE USER 'test_frappe'@'localhost' IDENTIFIED BY 'test_frappe'"
-    mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE DATABASE test_frappe"
-    mysql --host 127.0.0.1 --port 3306 -u root -e "GRANT ALL PRIVILEGES ON \`test_frappe\`.* TO 'test_frappe'@'localhost'"
-
-    mysql --host 127.0.0.1 --port 3306 -u root -e "UPDATE mysql.user SET Password=PASSWORD('travis') WHERE User='root'"
-    mysql --host 127.0.0.1 --port 3306 -u root -e "FLUSH PRIVILEGES"
-fi
-
-if [ "$DB" == "postgres" ];then
-    echo "travis" | psql -h 127.0.0.1 -p 5432 -c "CREATE DATABASE test_frappe" -U postgres;
-    echo "travis" | psql -h 127.0.0.1 -p 5432 -c "CREATE USER test_frappe WITH PASSWORD 'test_frappe'" -U postgres;
-fi
+mariadb --host 127.0.0.1 --port 3306 -u root -p123 -e "FLUSH PRIVILEGES";
 
 
-install_whktml() {
-    wget -O /tmp/wkhtmltox.tar.xz https://github.com/frappe/wkhtmltopdf/raw/master/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
-    tar -xf /tmp/wkhtmltox.tar.xz -C /tmp
-    sudo mv /tmp/wkhtmltox/bin/wkhtmltopdf /usr/local/bin/wkhtmltopdf
-    sudo chmod o+x /usr/local/bin/wkhtmltopdf
-}
-install_whktml &
+echo "Setting Up Procfile..."
 
-cd ~/frappe-bench || exit
+sed -i 's/^watch:/# watch:/g' Procfile
+sed -i 's/^schedule:/# schedule:/g' Procfile
 
-sed -i 's/watch:/# watch:/g' Procfile
-sed -i 's/schedule:/# schedule:/g' Procfile
-sed -i 's/socketio:/# socketio:/g' Procfile
-sed -i 's/redis_socketio:/# redis_socketio:/g' Procfile
+echo "Setting up redisearch module..."
+echo "loadmodule ${GITHUB_WORKSPACE}/.github/helper/redisearch.so" >> ./config/redis_cache.conf
+chmod +x "${GITHUB_WORKSPACE}/.github/helper/redisearch.so"
+cat ./config/redis_cache.conf
 
-bench get-app payments --branch ${githubbranch%"-hotfix"}
-bench get-app erpnext "${GITHUB_WORKSPACE}"
+echo "Starting Bench..."
 
-if [ "$TYPE" == "server" ]; then bench setup requirements --dev; fi
+bench start &> bench_start.log &
 
-bench start &> bench_run_logs.txt &
-CI=Yes bench build --app frappe &
+CI=Yes bench build &
+build_pid=$!
+
 bench --site test_site reinstall --yes
+# bench --site test_site install-app 
+
+# wait till assets are built succesfully
+wait $build_pid
