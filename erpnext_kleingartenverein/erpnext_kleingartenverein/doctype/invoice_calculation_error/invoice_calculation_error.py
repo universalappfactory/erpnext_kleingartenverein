8c2e9be511542234019b691c19e60beeb13bedce
dirk <info@universalappfactory.de>
2024-01-17 07:43:06 +0000

    feat: modified documents for invoice calculation

---
+++ erpnext_kleingartenverein/erpnext_kleingartenverein/doctype/invoice_calculation_error/invoice_calculation_error.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Kleingartenverein and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class InvoiceCalculationError(Document):
+	pass
